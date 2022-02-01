c62a51b64ad20ae3a7fe95e4f20e2898e85fd683
sandrarumi <rumisan332@yahoo.com>
2022-02-01 11:56:16 -0200

    criação quem somos

---
+++ quem-somos.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Casulo pra rua</title>
+
+<!-- Inserindo tags meta -->
+<meta name="description" content="Trajetória das idealizadoras do Casulo pra Rua">
+<meta name="keywords" content="casulo, Patrícia Curti, saco de dormir, Bibi Fragelli">
+
+
+<!-- Inserindo Favicon -->
+<link rel="shortcut icon" href="imagens/favicon.png" type="image/x-icon">
+
+ 
+<!-- CSS Externo -->
+<link rel="stylesheet" href="css/estilos.css">
+<link rel="stylesheet" href="css/quem-somos.css"
+
+</head>
+
+<header>
+
+    <div class="limitador">
+      <h1 class="logo">
+         <a href="index.html"><img src="imagens/logo.png" alt="logo Casulo pra rua"></a>
+      </h1>
+
+      <nav>
+          <h2 class="icone-menu">
+             <a href="" class="icone">Menu&equiv;</a>
+          </h2>
+          <ul class="menu">
+              <li><a href="index.html">Home</a></li>
+              <li><a href="">Cartilha</a></li>
+              <li><a href="">Nossos Parceiros</a></li>
+              <li><a href="">Fale conosco</a></li>
+          </ul>
+      </nav>
+
+    </div>
+</header>
+
+<article class="idealizadoras">
+    <h1>Quem somos</h1>
+    <p class="">O Casulo Pra Rua é um projeto criado pela estilista Bibi Fragelli, e pela sua sócia, a roterista, Patrícia Curti.</p>
+    <br>
+    <p>A iniciativa surgiu, mesmo antes da pandemia, de aliviar as noites frias de São Paulo, para a população de rua.</p>
+
+
+    <figure>
+        <img class="idealizadoras" src="imagens/idealizadoras.jpg" alt="Idealizadoras do Casulo para Rua - Bibi Fragelli e Patrícia Curti">
+        <figcaption>Idealizadoras Patrícia Curti e Bibi Fragelli</figcaption>
+    </figure>
+
+    <p>O projeto nasceu no dia 16 de abril de 2021 e tem feito muito sucesso. A intenção é que o Casulo seja replicado e multiplicado por todo Brasil. </p>  
+
+    <figure>
+        <img class="casulos" src="imagens/trio casulo.png" alt="Três ilustrações de casulos em tamanhos diferentes">
+        
+    </figure>
+
+    <p>O Casulo Pra Rua tem dois focos: beneficiar diretamente as pessoas em situação de rua e ao mesmo tempo, dar trabalho - com remuneração justa - para costureiras desempregadas ou de baixa renda.</p>  
+
+    <div class="frase">
+
+    </div>
+
+
+
+</article>
